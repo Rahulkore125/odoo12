7862e6e5fb9ec731855bd12a398240f41833765e
Vu Minh Tu (Odoo) <tuvm@magenest.com>
2019-11-09 16:10:15 +0700

    OHP-014: Auto confirm validate delivery after confirm SO manually by user

--- advanced_sale/models/sale_order.py
+++ advanced_sale/models/sale_order.py
@@ -1,6 +1,17 @@
-from odoo import models
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.multi
+    def action_confirm(self):
+        result = super(SaleOrder, self).action_confirm()
+        for so in self:
+            stock_pickings = self.env['stock.picking'].search(
+                [('sale_id', '=', so.id), ('picking_type_id.code', '=', 'outgoing')])
+            for stock_picking in stock_pickings:
+                for move_line in stock_picking.move_line_ids:
+                    move_line.qty_done = move_line.product_uom_qty
+                stock_picking.action_done()
+        return result
