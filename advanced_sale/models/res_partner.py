fadd48317200e966234651add739772ba4d6ec1c
Vu Minh Tu (Odoo) <tuvm@magenest.com>
2019-11-12 18:18:05 +0700

    OHP-019:add field title gender on form customer

--- advanced_sale/models/res_partner.py
+++ advanced_sale/models/res_partner.py
@@ -1,6 +1,8 @@
-from odoo import models
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    title_gender = fields.Selection(string="Title", selection=[('miss', 'Miss'), ('mister', 'Mister')])
 
