77e80f3a0e27199e9632ac2d7b53555e546d39ec
Vu Minh Tu (Odoo) <tuvm@magenest.com>
2019-12-06 20:16:04 +0700

    fix build

--- auto_install/models/models.py
+++ auto_install/models/models.py
@@ -6,8 +6,8 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    currency_id = fields.Many2one('res.currency', readonly=True,
-                                  default=lambda self: self.env.user.company_id.currency_id)
+    # currency_id = fields.Many2one('res.currency', readonly=True,
+    #                               default=lambda self: self.env.user.company_id.currency_id)
 
     def auto_install(self):
         # self.env['res.partner'].search([('create_date', '=', False)]).unlink()
