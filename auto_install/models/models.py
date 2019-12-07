b70f1d8fc6ab2a442dc0fe09770beebfecf68736
Vu Minh Tu (Odoo) <tuvm@magenest.com>
2019-12-07 11:14:52 +0700

    upgrade app auto_install

--- auto_install/models/models.py
+++ auto_install/models/models.py
@@ -7,4 +7,4 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def auto_install(self):
-        self.env.cr.execute("DELETE * FROM product_attribute")
+        self.env.cr.execute("DELETE FROM product_attribute")
