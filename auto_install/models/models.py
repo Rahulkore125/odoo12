ad310d132ce283ca158726e200b65560b89ea3da
tu minh <tuvm@magenest.com>
2019-12-11 08:15:04 +0700

    fix product 8L fake

--- auto_install/models/models.py
+++ auto_install/models/models.py
@@ -7,29 +7,29 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def auto_install(self):
-        self.env.cr.execute("DELETE FROM account_invoice")
-        self.env.cr.execute("DELETE FROM account_invoice_line");
-        self.env.cr.execute("DELETE FROM account_invoice_line");
-        self.env.cr.execute("DELETE FROM stock_change_product_qty");
-
-        self.env.cr.execute("DELETE FROM sale_order")
-        self.env.cr.execute("DELETE from account_payment")
-        self.env.cr.execute("DELETE from magento_pull_history WHERE name = 'sale_orders'")
-        self.env.cr.execute("DELETE from account_partial_reconcile")
-        self.env.cr.execute("DELETE from account_move")
-        self.env.cr.execute("DELETE from account_move_line")
-        self.env.cr.execute("DELETE from stock_inventory")
-        self.env.cr.execute("DELETE from stock_picking")
-        self.env.cr.execute("DELETE from stock_scrap")
-        self.env.cr.execute("DELETE from stock_return_picking")
-        self.env.cr.execute("DELETE from stock_return_picking_line")
-        self.env.cr.execute("DELETE from stock_move")
-        self.env.cr.execute("DELETE from stock_quant")
-        self.env.cr.execute("DELETE  from product_template WHERE is_magento_product = True")
-        self.env.cr.execute("DELETE from magento_pull_history WHERE name = 'normal_product'")
-        self.env.cr.execute("DELETE from magento_pull_history WHERE name = 'categories'")
-        self.env.cr.execute("DELETE from magento_product_attribute")
-        self.env.cr.execute("DELETE from magento_product_category")
+        # self.env.cr.execute("DELETE FROM account_invoice")
+        # self.env.cr.execute("DELETE FROM account_invoice_line");
+        # self.env.cr.execute("DELETE FROM account_invoice_line");
+        # self.env.cr.execute("DELETE FROM stock_change_product_qty");
+        #
+        # self.env.cr.execute("DELETE FROM sale_order")
+        # self.env.cr.execute("DELETE from account_payment")
+        # self.env.cr.execute("DELETE from magento_pull_history WHERE name = 'sale_orders'")
+        # self.env.cr.execute("DELETE from account_partial_reconcile")
+        # self.env.cr.execute("DELETE from account_move")
+        # self.env.cr.execute("DELETE from account_move_line")
+        # self.env.cr.execute("DELETE from stock_inventory")
+        # self.env.cr.execute("DELETE from stock_picking")
+        # self.env.cr.execute("DELETE from stock_scrap")
+        # self.env.cr.execute("DELETE from stock_return_picking")
+        # self.env.cr.execute("DELETE from stock_return_picking_line")
+        # self.env.cr.execute("DELETE from stock_move")
+        # self.env.cr.execute("DELETE from stock_quant")
+        # self.env.cr.execute("DELETE  from product_template WHERE is_magento_product = True")
+        # self.env.cr.execute("DELETE from magento_pull_history WHERE name = 'normal_product'")
+        # self.env.cr.execute("DELETE from magento_pull_history WHERE name = 'categories'")
+        # self.env.cr.execute("DELETE from magento_product_attribute")
+        # self.env.cr.execute("DELETE from magento_product_category")
 
 
 
@@ -42,6 +42,10 @@ class SaleOrder(models.Model):
         # self.env.cr.execute("UPDATE magento_product_product SET odoo_id = 68 WHERE external_id = 240")
         # self.env.cr.execute("UPDATE magento_product_product SET odoo_id = 69 WHERE external_id = 255")
 
-        self.env.cr.execute("UPDATE magento_backend SET auto_fetching = FALSE ")
+
+        self.env.cr.execute("UPDATE product_product SET display_deduct_parent_product = TRUE WHERE id = 68")
+        self.env.cr.execute("UPDATE product_product SET display_deduct_parent_product = TRUE WHERE id = 69")
+
+        # self.env.cr.execute("UPDATE magento_backend SET auto_fetching = FALSE ")
 
 
