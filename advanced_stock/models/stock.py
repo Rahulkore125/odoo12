1eafe302875412b40a41bd93fa83fcdf2fd7bf61
tu minh <tuvm@magenest.com>
2019-12-18 22:33:05 +0700

    OHP-041: fix update stock of product 8L keg when you update it by file excel

--- advanced_stock/models/stock.py
+++ advanced_stock/models/stock.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api,tools,_
+from odoo import models, fields, api, tools, _
+from odoo.tools import float_utils, float_compare
 
 
 class ProductChangeQuantity(models.TransientModel):
@@ -42,3 +43,61 @@ class Inventory(models.Model):
         self.write({'state': 'done'})
         self.post_inventory()
         return True
+
+    def action_validate(self):
+        inventory_lines = self.line_ids.filtered(lambda l: l.product_id.tracking in ['lot',
+                                                                                     'serial'] and not l.prod_lot_id and l.theoretical_qty != l.product_qty)
+        print(inventory_lines)
+        lines = self.line_ids.filtered(lambda l: float_compare(l.product_qty, 1,
+                                                               precision_rounding=l.product_uom_id.rounding) > 0 and l.product_id.tracking == 'serial' and l.prod_lot_id)
+        print(lines)
+        if inventory_lines and not lines:
+            wiz_lines = [(0, 0, {'product_id': product.id, 'tracking': product.tracking}) for product in
+                         inventory_lines.mapped('product_id')]
+            wiz = self.env['stock.track.confirmation'].create({'inventory_id': self.id, 'tracking_line_ids': wiz_lines})
+            return {
+                'name': _('Tracked Products in Inventory Adjustment'),
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'stock.track.confirmation',
+                'target': 'new',
+                'res_id': wiz.id,
+            }
+        else:
+            self._action_done()
+
+    def action_check(self):
+        """ Checks the inventory and computes the stock move to do """
+        # tde todo: clean after _generate_moves
+        for inventory in self.filtered(lambda x: x.state not in ('done', 'cancel')):
+            # first remove the existing stock moves linked to this inventory
+            inventory.mapped('move_ids').unlink()
+            inventory.line_ids._generate_moves()
+            print(inventory.line_ids)
+            for e in inventory.line_ids:
+                if e.product_id.product_tmpl_id.multiple_sku_one_stock:
+                    print(e.product_id.product_tmpl_id.variant_manage_stock.id)
+
+                    if e.product_id.id == e.product_id.product_tmpl_id.variant_manage_stock.id:
+                        e.product_id.product_tmpl_id.origin_quantity = e.product_qty
+
+
+class InventoryLine(models.Model):
+    _inherit = 'stock.inventory.line'
+
+    def _generate_moves(self):
+        vals_list = []
+        for line in self:
+
+            if float_utils.float_compare(line.theoretical_qty, line.product_qty,
+                                         precision_rounding=line.product_id.uom_id.rounding) == 0:
+                continue
+            diff = line.theoretical_qty - line.product_qty
+            if diff < 0:  # found more than expected
+                vals = line._get_move_values(abs(diff), line.product_id.property_stock_inventory.id,
+                                             line.location_id.id, False)
+            else:
+                vals = line._get_move_values(abs(diff), line.location_id.id,
+                                             line.product_id.property_stock_inventory.id, True)
+            vals_list.append(vals)
+        return self.env['stock.move'].create(vals_list)
