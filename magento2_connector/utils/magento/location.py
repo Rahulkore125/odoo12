89ccfe615e5a165c9c9c5be9697071edbaa0e6ed
Vu Minh Tu (Odoo) <tuvm@magenest.com>
2020-03-25 18:44:38 +0700

    OHP-051: add parent location on location be pulled from magento

--- magento2_connector/utils/magento/location.py
+++ magento2_connector/utils/magento/location.py
@@ -11,6 +11,8 @@ class Location(Client):
         for source in sources:
             odoo_location = context.env['stock.location'].search(
                 [('is_from_magento', '=', True), ('magento_source_code', '=', source['source_code'])])
+            warehouse_location = context.env['stock.warehouse'].search([]).view_location_id
+
             if len(odoo_location) > 0:
                 odoo_location.write({
                     'name': source['name'],
@@ -18,7 +20,8 @@ class Location(Client):
                     'magento_source_code': source['source_code'],
                     'type': 'internal',
                     'postcode': source['postcode'],
-                    'active': source['enabled']
+                    'active': source['enabled'],
+                    'location_id': warehouse_location.id
                 })
             else:
                 odoo_location = context.env['stock.location'].create({
@@ -27,7 +30,8 @@ class Location(Client):
                     'magento_source_code': source['source_code'],
                     'type': 'internal',
                     'postcode': source['postcode'],
-                    'active': source['enabled']
+                    'active': source['enabled'],
+                    'location_id': warehouse_location.id
                 })
 
                 context.env['magento.source'].create({
